586fbaac9fc6e6e0e1f8e51f134dfe26e09912fd
Gabriel Gondim <69263111+Gabrielfsg@users.noreply.github.com>
2023-05-24 20:08:30 -0300

    Update questao9.asm

--- questao9.asm
+++ questao9.asm
@@ -2,8 +2,8 @@
 
 	num1: .word 10
 	num2: .word 6
-	msgIgual: .asciiz "Os números são iguais."
-	msgdif: .asciiz "Os números são diferentes."
+	msgIgual: .asciiz "Os nÃºmeros sÃ£o iguais."
+	msgdif: .asciiz "Os nÃºmeros sÃ£o diferentes."
 	
 .text
 	
@@ -32,3 +32,47 @@
 		
 		li $v0, 10
 		syscall
+
+
+.data
+
+	a: .word 0,1,2,3,4,5
+	valor: .asciiz "O valor de a[i] Ã©: "
+	quebra: .asciiz "\n"
+
+
+.text
+
+	li $s0, 0
+	la $s6, a
+
+	loop:
+	
+		beq $s0, 6, sair
+	
+		sll $t0, $s0, 2
+		add $t0, $t0, $s6
+		lw $t1, 0($t0)
+	
+		li $v0, 4
+		la $a0 valor
+		syscall
+		
+		li $v0, 1
+		move $a0, $t1
+		syscall
+		
+		li $v0, 4
+		la $a0 quebra
+		syscall
+		
+		addi $s0, $s0, 1
+		
+		j loop
+		
+	sair:
+	
+		li $v0, 10
+		syscall 
+	
+		
